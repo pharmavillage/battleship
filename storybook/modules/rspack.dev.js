74a198928283b0c40f6c78c3ef4d574c75a3598a
Hossam sulleman <sullemanhossam4@gmail.com>
2024-07-09 11:43:35 +0100

    Push

---
+++ storybook/modules/rspack.dev.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
+const { ModuleFederationPlugin } = require("@rspack/core").container;
+const common = require("./webpack.common.js");
+// const { rspack } = require("@rspack/core");
+
+delete common.plugins;
+module.exports = merge(common, {
+  experiments: {
+    css: true,
+  },
+  mode: "development",
+  entry: "./src",
+  plugins: [
+    // new pluginLightningcss(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ModuleFederationPlugin({
+      name: "remoteLibrary",
+      filename: "remoteEntry.js",
+      exposes: {
+        // "./Button": "./src/components/Button.jsx",
+        // "./Header": "./src/components/Header.jsx",
+      },
+    }),
+  ],
+  devServer: {
+    port: 49155,
+    hot: true,
+    compress: true,
+    watchFiles: ["src/**/*"],
+  },
+});
